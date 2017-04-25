e574af628ff3a85f958be44f10b2127fd98e224f
Michael Le <mjle@ucsc.edu>
2017-04-24 22:15:52 -0700

    pod files and apis

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your 
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'rainyshinycloudy' do
+pod 'Alamofire', '~> 4.4'
+
+end
--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+let BASE_URL = "http://samples.openweathermap.org/data/2.5/"
+let API_KEY = "f247900a910a1be005ea93d0bb0c2a06"
+
+typealias DownloadComplete = () -> ()
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)weather?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&appid=\(API_KEY)"
+let FORECAST_URL = "\(BASE_URL)forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&appid=\(API_KEY)"
+
+// Sample URLs
+// http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f247900a910a1be005ea93d0bb0c2a06
+//https://samples.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&appid=f247900a910a1be005ea93d0bb0c2a06
